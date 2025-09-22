82937b147c0edfcc56001a53b2272074806d8c66
Mikhail Karasev <seplaz38@yandex.ru>
2025-09-22 09:59:45 +0800

    feat: Исправлены замечания ревьюера

--- cypress/e2e/constructor.cy.ts
+++ cypress/e2e/constructor.cy.ts
@@ -1,3 +1,5 @@
+import { SELECTORS, INGREDIENT_NAMES } from '../support/selectors';
+
 describe('Тестирование конструктора бургеров, модальных окон и создания заказа', () => {
   beforeEach(() => {
     cy.intercept('GET', 'api/ingredients', {
@@ -15,7 +17,7 @@ describe('Тестирование конструктора бургеров, м
     window.localStorage.setItem('refreshToken', 'testRefreshToken');
     cy.setCookie('accessToken', 'testAccessToken');
 
-    cy.visit('localhost:4000');
+    cy.visit('/');
     cy.wait('@getIngredients');
   });
 
@@ -26,139 +28,96 @@ describe('Тестирование конструктора бургеров, м
 
   describe('Тестирование конструктора бургеров', () => {
     it('Тест на добавление булки в конструктор', () => {
-      cy.get('[data-cy="ingredient-643d69a5c3f7b9001cfa093c"] button').click();
-
-      cy.get('[data-cy="constructor-bun-top"]')
-        .should('be.visible')
-        .and('contain', 'Краторная булка N-200i');
-
-      cy.get('[data-cy="constructor-bun-bottom"]')
-        .should('be.visible')
-        .and('contain', 'Краторная булка N-200i');
+      cy.addBunToConstructor();
+      cy.verifyConstructorBuns(INGREDIENT_NAMES.BUN);
     });
 
     it('Тест на добавление начинки в конструктор', () => {
-      cy.get('[data-cy="ingredient-643d69a5c3f7b9001cfa0941"] button').click();
-
-      cy.get('[data-cy="constructor-main"]')
-        .should('be.visible')
-        .and('contain', 'Биокотлета из марсианской Магнолии');
+      cy.addMainToConstructor();
+      cy.verifyConstructorIngredient(INGREDIENT_NAMES.MAIN);
     });
 
     it('Тест на добавление соуса в конструктор', () => {
-      cy.get('[data-cy="ingredient-643d69a5c3f7b9001cfa0942"] button').click();
-
-      cy.get('[data-cy="constructor-main"]')
-        .should('be.visible')
-        .and('contain', 'Соус Spicy-X');
+      cy.addSauceToConstructor();
+      cy.verifyConstructorIngredient(INGREDIENT_NAMES.SAUCE);
     });
 
     it('Тест на добавление всех ингредиентов бургера', () => {
-      cy.get('[data-cy="ingredient-643d69a5c3f7b9001cfa093c"] button').click();
-      cy.get('[data-cy="ingredient-643d69a5c3f7b9001cfa0941"] button').click();
-      cy.get('[data-cy="ingredient-643d69a5c3f7b9001cfa0942"] button').click();
+      cy.addBunToConstructor();
+      cy.addMainToConstructor();
+      cy.addSauceToConstructor();
 
-      cy.get('[data-cy="constructor-bun-top"]')
-        .should('be.visible')
-        .and('contain', 'Краторная булка N-200i');
-
-      cy.get('[data-cy="constructor-bun-bottom"]')
-        .should('be.visible')
-        .and('contain', 'Краторная булка N-200i');
-
-      cy.get('[data-cy="constructor-main"]')
-        .should('be.visible')
-        .and('contain', 'Биокотлета из марсианской Магнолии');
-
-      cy.get('[data-cy="constructor-main"]')
-        .should('be.visible')
-        .and('contain', 'Соус Spicy-X');
+      cy.verifyConstructorBuns(INGREDIENT_NAMES.BUN);
+      cy.verifyConstructorIngredient(INGREDIENT_NAMES.MAIN);
+      cy.verifyConstructorIngredient(INGREDIENT_NAMES.SAUCE);
     });
   });
 
   describe('Тестирование модальных окон', () => {
     it('Открытие и закрытие (по крестику и оверлею) модального окна булки', () => {
-      cy.get('[data-cy="ingredient-643d69a5c3f7b9001cfa093c"]').click();
+      cy.openIngredientModal('bun');
 
-      cy.get('[data-cy="modal"]')
+      cy.get(SELECTORS.MODAL)
+        .as('modal')
         .should('be.visible')
-        .and('contain', 'Краторная булка N-200i');
+        .and('contain', INGREDIENT_NAMES.BUN);
 
-      cy.get('[data-cy="close-modal-button"]').click();
-      cy.get('[data-cy="modal"]').should('not.exist');
+      cy.closeModalByButton();
 
-      cy.get('[data-cy="ingredient-643d69a5c3f7b9001cfa093c"]').click();
-      cy.get('[data-cy="close-modal-overlay"]').click({ force: true });
-      cy.get('[data-cy="modal"]').should('not.exist');
+      cy.openIngredientModal('bun');
+      cy.closeModalByOverlay();
     });
 
     it('Открытие и закрытие (по крестику и оверлею) модального окна ингредиента', () => {
-      cy.get('[data-cy="ingredient-643d69a5c3f7b9001cfa0941"]').click();
+      cy.openIngredientModal('main');
 
-      cy.get('[data-cy="modal"]')
+      cy.get(SELECTORS.MODAL)
+        .as('modal')
         .should('be.visible')
-        .and('contain', 'Биокотлета из марсианской Магнолии');
+        .and('contain', INGREDIENT_NAMES.MAIN);
 
-      cy.get('[data-cy="close-modal-button"]').click();
-      cy.get('[data-cy="modal"]').should('not.exist');
+      cy.closeModalByButton();
 
-      cy.get('[data-cy="ingredient-643d69a5c3f7b9001cfa0941"]').click();
-      cy.get('[data-cy="close-modal-overlay"]').click({ force: true });
-      cy.get('[data-cy="modal"]').should('not.exist');
+      cy.openIngredientModal('main');
+      cy.closeModalByOverlay();
     });
 
     it('Открытие и закрытие (по крестику и оверлею) модального окна соуса', () => {
-      cy.get('[data-cy="ingredient-643d69a5c3f7b9001cfa0942"]').click();
+      cy.openIngredientModal('sauce');
 
-      cy.get('[data-cy="modal"]')
+      cy.get(SELECTORS.MODAL)
+        .as('modal')
         .should('be.visible')
-        .and('contain', 'Соус Spicy-X');
+        .and('contain', INGREDIENT_NAMES.SAUCE);
 
-      cy.get('[data-cy="close-modal-button"]').click();
-      cy.get('[data-cy="modal"]').should('not.exist');
+      cy.closeModalByButton();
 
-      cy.get('[data-cy="ingredient-643d69a5c3f7b9001cfa0942"]').click();
-      cy.get('[data-cy="close-modal-overlay"]').click({ force: true });
-      cy.get('[data-cy="modal"]').should('not.exist');
+      cy.openIngredientModal('sauce');
+      cy.closeModalByOverlay();
     });
   });
 
   describe('Тестирование создания заказа', () => {
     it('Создание заказа с проверкой модального окна и очистки конструктора', () => {
-      cy.get('[data-cy="ingredient-643d69a5c3f7b9001cfa093c"] button').click();
-      cy.get('[data-cy="ingredient-643d69a5c3f7b9001cfa0941"] button').click();
-      cy.get('[data-cy="ingredient-643d69a5c3f7b9001cfa0942"] button').click();
+      cy.addBunToConstructor();
+      cy.addMainToConstructor();
+      cy.addSauceToConstructor();
 
-      cy.get('[data-cy="constructor-bun-top"]')
-        .should('be.visible')
-        .and('contain', 'Краторная булка N-200i');
+      cy.verifyConstructorBuns(INGREDIENT_NAMES.BUN);
+      cy.verifyConstructorIngredient(INGREDIENT_NAMES.MAIN);
+      cy.verifyConstructorIngredient(INGREDIENT_NAMES.SAUCE);
 
-      cy.get('[data-cy="constructor-main"]')
-        .should('be.visible')
-        .and('contain', 'Биокотлета из марсианской Магнолии');
-
-      cy.get('[data-cy="constructor-main"]')
-        .should('be.visible')
-        .and('contain', 'Соус Spicy-X');
-
-      cy.get('[data-cy="constructor-bun-bottom"]')
-        .should('be.visible')
-        .and('contain', 'Краторная булка N-200i');
-
-      cy.get('[data-cy="constructor-order-button"]').click();
+      cy.get(SELECTORS.CONSTRUCTOR_ORDER_BUTTON).click();
       cy.wait('@createOrder');
 
-      cy.get('[data-cy="modal"]').should('be.visible').and('contain', '12345');
+      cy.get(SELECTORS.MODAL)
+        .as('modal')
+        .should('be.visible')
+        .and('contain', '12345');
 
-      cy.get('[data-cy="close-modal-button"]').click();
-      cy.get('[data-cy="modal"]').should('not.exist');
+      cy.closeModalByButton();
 
-      cy.get('[data-cy="constructor-bun-top"]').should('not.exist');
-      cy.get('[data-cy="constructor-main"]').should(
-        'contain',
-        'Выберите начинку'
-      );
-      cy.get('[data-cy="constructor-bun-bottom"]').should('not.exist');
+      cy.verifyEmptyConstructor();
     });
   });
 });
--- cypress/support/commands.ts
+++ cypress/support/commands.ts
@@ -1,37 +1,78 @@
 /// <reference types="cypress" />
-// ***********************************************
-// This example commands.ts shows you how to
-// create various custom commands and overwrite
-// existing commands.
-//
-// For more comprehensive examples of custom
-// commands please read more here:
-// https://on.cypress.io/custom-commands
-// ***********************************************
-//
-//
-// -- This is a parent command --
-// Cypress.Commands.add('login', (email, password) => { ... })
-//
-//
-// -- This is a child command --
-// Cypress.Commands.add('drag', { prevSubject: 'element'}, (subject, options) => { ... })
-//
-//
-// -- This is a dual command --
-// Cypress.Commands.add('dismiss', { prevSubject: 'optional'}, (subject, options) => { ... })
-//
-//
-// -- This will overwrite an existing command --
-// Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
-//
-// declare global {
-//   namespace Cypress {
-//     interface Chainable {
-//       login(email: string, password: string): Chainable<void>
-//       drag(subject: string, options?: Partial<TypeOptions>): Chainable<Element>
-//       dismiss(subject: string, options?: Partial<TypeOptions>): Chainable<Element>
-//       visit(originalFn: CommandOriginalFn, url: string, options: Partial<VisitOptions>): Chainable<Element>
-//     }
-//   }
-// }
+import { SELECTORS } from './selectors';
+
+Cypress.Commands.add('addBunToConstructor', () => {
+  cy.get(`${SELECTORS.INGREDIENT_BUN} button`).click();
+});
+
+Cypress.Commands.add('addMainToConstructor', () => {
+  cy.get(`${SELECTORS.INGREDIENT_MAIN} button`).click();
+});
+
+Cypress.Commands.add('addSauceToConstructor', () => {
+  cy.get(`${SELECTORS.INGREDIENT_SAUCE} button`).click();
+});
+
+Cypress.Commands.add(
+  'openIngredientModal',
+  (ingredientType: 'bun' | 'main' | 'sauce') => {
+    const selectors = {
+      bun: SELECTORS.INGREDIENT_BUN,
+      main: SELECTORS.INGREDIENT_MAIN,
+      sauce: SELECTORS.INGREDIENT_SAUCE
+    };
+    cy.get(selectors[ingredientType]).click();
+  }
+);
+
+Cypress.Commands.add('closeModalByButton', () => {
+  cy.get(SELECTORS.CLOSE_MODAL_BUTTON).click();
+  cy.get(SELECTORS.MODAL).should('not.exist');
+});
+
+Cypress.Commands.add('closeModalByOverlay', () => {
+  cy.get(SELECTORS.CLOSE_MODAL_OVERLAY).click({ force: true });
+  cy.get(SELECTORS.MODAL).should('not.exist');
+});
+
+Cypress.Commands.add('verifyConstructorBuns', (bunName: string) => {
+  cy.get(SELECTORS.CONSTRUCTOR_BUN_TOP)
+    .should('be.visible')
+    .and('contain', bunName);
+  cy.get(SELECTORS.CONSTRUCTOR_BUN_BOTTOM)
+    .should('be.visible')
+    .and('contain', bunName);
+});
+
+Cypress.Commands.add(
+  'verifyConstructorIngredient',
+  (ingredientName: string) => {
+    cy.get(SELECTORS.CONSTRUCTOR_MAIN)
+      .should('be.visible')
+      .and('contain', ingredientName);
+  }
+);
+
+Cypress.Commands.add('verifyEmptyConstructor', () => {
+  cy.get(SELECTORS.CONSTRUCTOR_BUN_TOP).should('not.exist');
+  cy.get(SELECTORS.CONSTRUCTOR_BUN_BOTTOM).should('not.exist');
+  cy.get(SELECTORS.CONSTRUCTOR_MAIN).should('contain', 'Выберите начинку');
+});
+
+declare global {
+  namespace Cypress {
+    interface Chainable {
+      addBunToConstructor(): Chainable<void>;
+      addMainToConstructor(): Chainable<void>;
+      addSauceToConstructor(): Chainable<void>;
+      openIngredientModal(
+        ingredientType: 'bun' | 'main' | 'sauce'
+      ): Chainable<void>;
+      closeModalByButton(): Chainable<void>;
+      closeModalByOverlay(): Chainable<void>;
+      verifyConstructorBuns(bunName: string): Chainable<void>;
+      verifyConstructorIngredient(ingredientName: string): Chainable<void>;
+      verifyEmptyConstructor(): Chainable<void>;
+    }
+  }
+}
